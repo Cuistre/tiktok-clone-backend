9f1a6f5624ff67ac24ab4ad38f7f24de4e6fc02e
Gaultier Hym <gaultier.hym@gmail.com>
2021-11-24 23:01:18 +0100

    Removed node_module folder

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    url: 'https://v77.tiktokcdn.com/6fcecd0f0749c14b1243d2cb9ad8cbb5/619ea4da/video/tos/useast2a/tos-useast2a-pve-0068/0b8e64b163a84149b65bf5f7117d73a2/?a=1233&br=2332&bt=1166&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=wZ~R8F8rkag3-I&l=2021112414471101019216101507234051&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=amczOjg6ZnFpOTMzNzczM0ApNjhpOjY4NWU8N2Y1ODtkZGdgMC9qcjRnNjJgLS1kMTZzcy8tMjVjYDVgYWBfM2A1NjE6Yw%3D%3D&vl=&vr=',
+    channel: 'myGuy',
+    description: "You'll get a surprise",
+    song: 'Epic cats',
+    likes: '978',
+    messages: '120',
+    shares: '64',
+  },
+  {
+    url: 'https://v77.tiktokcdn.com/658b1b67d5dc4de10743765f2d543b0c/619ea4dd/video/tos/useast2a/tos-useast2a-ve-0068c003/58cd2fe13ba344e3beca746b4cf826d7/?a=1233&br=2480&bt=1240&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=wZ~R8F8rkag3-I&l=2021112414471101019216101507234051&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M2o8PGU6Zjg3OTMzNzczM0ApPGUzZjZnaTw2N2U0OTplOWdkbDRzcjRvaTBgLS1kMTZzczNhNC4tMi82MTReM18xYC06Yw%3D%3D&vl=&vr=',
+    channel: 'DriverZZ',
+    description: 'Me driving',
+    song: 'Nice trip',
+    likes: '1540',
+    messages: '420',
+    shares: '73',
+  },
+];
+
+export default data;
